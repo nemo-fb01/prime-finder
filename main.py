1bf629d95312f2f3bfd1f1461c49d57b07225c01
nemo-fb01 <nemo.fb01@outlook.com>
2026-02-11 21:06:32 +0000

    update

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from spin_sdk import http
-from spin_sdk.http import Request, Response
+from spin_sdk.http import IncomingHandler, Request, Response
 
-class IncomingHandler(http.IncomingHandler):
+class IncomingHandler(IncomingHandler):
     def handle_request(self, request: Request) -> Response:
-        # Giả sử đây là nơi bạn thực hiện logic Prime Finder
-        # Bạn có thể gọi hàm xử lý của bạn tại đây
-        
-        return Response(
-            200,
-            {"content-type": "text/plain"},
-            bytes("Kết quả tìm số nguyên tố từ Spin Cloud!", "utf-8")
-        )
+        return Response(200, {"content-type": "text/plain"}, b"Hello Spin")
